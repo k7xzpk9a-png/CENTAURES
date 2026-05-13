bffc9bbac85aa26bc0ea8626e5654269392d41b6
phatomhma <phatomhma@gmail.com>
2026-05-13 17:24:58 +0200

    Rebuild with correct BASE_PATH=/CENTAURES

---
+++ docs/_app/immutable/entry/start.42hi3fQe.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D_5XHciZ.js";import{y as t}from"../chunks/I337T29D.js";export{t as load_css,r as start};
--- docs/_app/immutable/entry/start.DQXFc1bK.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BLciWtc8.js";import{y as t}from"../chunks/DZ8kGtow.js";export{t as load_css,r as start};
